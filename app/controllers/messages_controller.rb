d5464c4e41c4cf53643b594a9ec2d381e31d085c
Priyank Gupta <priyaaank@gmail.com>
2011-06-26 16:47:31 -0400

    Added some more code, to pull a message to chat area from server, or error message if posting failed. Will probably change the flow; as user message is better posted immediatly. However I also need to organize javascript code better. It'll grow into a mess this way.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,17 @@
 class MessagesController < ApplicationController
 
+  before_filter :load_room
+
   def index
-    room = Room.find(params[:room_id])
-    @messages = room.messages.all_messages_after(Time.zone.now.beginning_of_day) if room
+    @messages = @room.messages.all_messages_after(Time.zone.now.beginning_of_day) if @room
     respond_to do |format|
       format.html {render 'index'}
     end
   end
 
   def show
-    room = Room.find(params[:room_id]) 
     if last_known_id = params[:id]
-      @messages = room.messages.all_messages_from(last_known_id)
+      @messages = @room.messages.all_messages_from(last_known_id)
     end
     respond_to do |format|
       format.js {render :show}
@@ -20,21 +20,20 @@ class MessagesController < ApplicationController
 
   def create
     message = params[:message]
-    room = Room.find(params[:room_id])
     if message.present?
-      if current_user.messages.build(:text => message, :room => room).save
-        render :status => :ok, :json => {:message => message}
+      new_message = current_user.messages.build(:text => message, :room => @room)
+      if new_message.save
+        render :status => :ok, :json => { :sender => new_message.user.email,
+                                          :message => new_message.text}
       else
-        render :status => 400, :json => error_json 
+        render :status => :unprocessable_entity, :text => "Your message was not received by others!"
       end
     end
   end
 
-
   private
 
-  def error_json
-    {:error => "Your message was not received by others!"}
+  def load_room
+    @room ||= Room.find(params[:room_id])
   end
-
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,16 +11,26 @@ $(document).ready(function() {
   //BEGIN of message object
 
   messages = {
-
     fetch: function() {
       $.get("http://localhost:3000/rooms/1/messages/1.js", null, null, "script");
     },
 
-    send: function(url, data) {
-      $.post(url, data, this.success);
+    attach: function(sender, message) {
+      dom_structure =  "<div class='message'><div class='block'><div class='sender'><h6>"+
+                       sender +
+                       "</h6></div><div class='text'><h6>" +
+                       message +
+                       "</h6></div><div class='controls'><h6>"+
+                       "Just Now" +
+                       "</h6></div></div></div>"
+      $("#chat-area").append(dom_structure);
     },
 
-    success: function(data) {
+    send: function(url, data) {
+      $.post(url, data, null)
+        .success(function(data) { messages.attach(data.sender, data.message); })
+        .error(function(error) { messages.attach("Server", error.responseText); }
+      );
     },
   };
  
